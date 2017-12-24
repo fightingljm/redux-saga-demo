9961ab698e111f7a7dcf86bfb4d7b31e85b8c45f
wolf_liu <1321839748@qq.com>
2017-12-24 17:35:07 +0800

    test

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-基于 create-react-app 的 react 脚手架代码。
-
-```
-npm i redux redux-thunk redux-logger styled-components
-```
+# redux-saga
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,21 @@ import axios from 'axios'
 import { POSTS_URL } from '../constants/ApiConstants'
 import * as types from '../constants/ActionTypes'
 
-export const loadPosts = () => dispatch => {
-  axios.get(POSTS_URL).then(
-    res => {
-      dispatch({
-        type: types.LOAD_POSTS,
-        posts: res.data
-      })
-    }
-  )
+// export const loadPosts = () => dispatch => {
+//   axios.get(POSTS_URL).then(
+//     res => {
+//       dispatch({
+//         type: types.LOAD_POSTS,
+//         posts: res.data
+//       })
+//     }
+//   )
+// }
+
+export const fetchPostsRequest = () =>({
+    type: types.FETCH_POSTS_REQUEST
+})
+
+export const fetchPosts = () => {
+    console.log('fetchPosts...执行异步操作')
 }
